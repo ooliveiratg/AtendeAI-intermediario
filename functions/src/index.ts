7647f8bb862a3470c8554ecc30cb855ae9f08954
ooliveiratg <menezesth16@gmail.com>
2026-09-04 23:56:38 -0300

    fix: prevent previous user session from persisting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,15 +17,13 @@ export const ping = onCall(() => {
  * dependendo do nível do teste que você recebeu.
  */
 export const listAtendimentos = onCall(async (request) => {
-  console.log("TEEEEEEEEEEEEEEEESTE: ",request.auth)
+  console.log(request)
+  const tenantId = request.auth?.token.tenantId;
+  console.log(tenantId)
 
-  console.log("DATTAAAAA: ",request.data)
-  const tenantId = request.data?.tenantId;
-  console.log("TEEEEEEEEEEEEEEEESTE: ",request.auth)
   if (!tenantId || typeof tenantId !== "string") {
     throw new HttpsError("invalid-argument", "tenantId é obrigatório.");
   }
-  console.log("TEEEEEEEEEEEEEEEESTE: ",request.auth)
   const snapshot = await db
     .collection("atendimentos")
     .where("tenantId", "==", tenantId)
